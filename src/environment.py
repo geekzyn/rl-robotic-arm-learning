37c0c706e479964f470221a705a962b189d67249
Ali <abdelali.ezzyn@gmail.com>
2021-01-12 14:56:39 +0000

    edited simulation task

--- src/environment.py
+++ src/environment.py
@@ -15,14 +15,13 @@ POS_MIN, POS_MAX = [0.1, -0.3, 1.], [0.45, 0.3, 1.] # valid position range of ta
 
 class GraspEnv(object):
     """Sawyer robot grasping a cuboid """ 
-    def __init__(self, headless, control_mode='joint_velocity'):
+    def __init__(self, headless):
         """
         Initiliaze the environment, setting the public variables, launching and setting up the scene, setting 
         the porxy variables for the counterparts in the scene
 
         Args:
             headless (bool): if True, no visualization, else with visualization
-            control_mode (str, optional): 'end_position' or 'joint_velocity'. Defaults to 'joint_velocity'.
         """
         # set public variables
         self.headless = headless
@@ -31,55 +30,36 @@ class GraspEnv(object):
         self.penalty_offset = 1. # penalty value for undesired cases
         self.fall_down_offset = 0.1 # distance for judging the target object fall off the table
         self.metadata = [] # gym env argument
-        self.control_mode = control_mode
 
         # launch and setup scene
+        SCENE_FILE = join(dirname, (abspath(__file__)), './simulations/sawyer_reacher.ttt') # scene with joints controlled by FK
         self.pr = PyRep() # call the PyRep
-        if control_mode == 'end_position':
-            # the controle mode with all joints in inverse kinematics mode
-            SCENE_FILE = join(dirname, (abspath(__file__)), './simulations/sawyer_reacher_rl_new_ik.ttt') # scene with joints controlled by IK
-        elif control_mode == 'joint_velocity':
-            SCENE_FILE = join(dirname, (abspath(__file__)), './simulations/sawyer_reacher_rl_new.ttt') # scene with joints controlled by FK
         self.pr.launch(SCENE_FILE, headless = self.headless) # launch the scene
         self.pr.start() # start the scene
 
         # set proxy variables for the counterparts in the scene
         self.agent = Sawyer() # get robot arm in the scene
-        self.gripper = BaxterGripper # get the gripper in the scene
+        self.gripper = BaxterGripper() # get the gripper in the scene
         self.gripper_left_pad = Shape('BaxterGripper_leftPad') # name of the left pad of the gripper finger
         self.proximity_sensor = ProximitySensor('BaxterGripper_attachProxSensor') # name of the proximity sensor
         self.vision_sensor = VisionSensor('Vision_sensor') # name of the vision sensor
         self.table = Shape('diningTable') # name of the table in the scene, for checking collision
 
         # set action and observation space
-        if control_mode == 'end_position':
-            # control the robot arm by the position of its end using IK
-            self.agent.set_control_loop_enabled(True) # if False, IK won't work
-            self.action_space = np.zeros(4) # 3 DOF end position control + 1 DOF rotation of gripper
-        elif control_mode == 'joint_velocity':
-            # control the robot arm by directly setting velocity values on each joint, using FK
-            self.agent.set_control_loop_enabled(False)
-            self.action_space = np.zeros(7) # 7 DOF velocity control, no need for extra control of the end rotation, 7th joint controls it
-        else:
-            raise NotImplementedError
+        self.agent.set_control_loop_enabled(False)
+        self.action_space = np.zeros(7) # 7 DOF velocity control, no need for extra control of the end rotation, 7th joint controls it
         self.observation_space = np.zeros(17) # scalar positions and scalar velocities of 7 joints + 3-dimensional position of the target
         self.agent.set_motor_locked_at_zero_velocity(True) # joints locked in place when velocity is zero
 
         # set agent tip and target position for IK chain
         self.target = Shape('target') # get target object
         self.agent_ee_tip = self.agent.get_tip() # a part of robot as the end of IK chain for controlling
-        self.tip_target = Dummy('Sawyer_target') # the target point of the tip (end of the robot arm) to move towards
+        self.tip_target = Dummy('Sawyer_target') # the target point of the tip (end of the robot arm) to move towards /
         self.tip_pos = self.agent_ee_tip.get_position() # tip x, y, z position
 
-        # set proper initial robot gesture or tip position
-        if control_mode == 'end_position':
-            initial_pos = [0.3, 0.1, 0.9]
-            self.tip_target.set_position(initial_pos) # set target position
-            # one big step for rotation setting is enough, with reset_dynamics = True, set the rotation instantaneously
-            self.tip_target.set_orientation([0, np.pi, np.pi/2], reset_dynamics=True) # first two dimensions along x, y axis make gripper face downwards
-        elif control_mode == 'joint_velocity':
-            self.initial_joint_positions = [0.0, -1.4, 0.7, 2.5, 3.0, -0.5, 4.1] # a proper initial gesture
-            self.agent.set_joint_positions(self.initial_joint_positions)
+        # set proper initial robot gesture
+        self.initial_joint_positions = [0.0, -1.4, 0.7, 2.5, 3.0, -0.5, 4.1] # a proper initial gesture
+        self.agent.set_joint_positions(self.initial_joint_positions)
         
         self.pr.step() # Step the physics simulation
         self.initial_tip_positions = self.agent_ee_tip.get_position()
@@ -106,47 +86,111 @@ class GraspEnv(object):
         else:
             return False
 
-    def _move(self, action, bounding_offset = 0.15, step_factor = 0.2, max_iter = 20, max_error = 0.05, rotation_norm = 5.):
-        """Move the tip according to the action with the IK for 'end_position' controle mode. IK mode control is archieved through
-        setting the tip target insead of using .solve_ik(), because sometimes the .solve_ik() does not function correctly.
-        Mode: close-loop proportional control, using IK
-
-        Args:
-            action ([type]): [description]
-            bounding_offset (float, optional): offset of bounding box outside the valid target position range, as valid and safe range of action. Defaults to 0.15.
-            step_factor (float, optional): small step factor multiplied on the difference of current and desired positions, i.e. proportional factor. Defaults to 0.2.
-            max_iter (int, optional): maximum moving iterations. Defaults to 20.
-            max_error (float, optional): upper bound of distance error for movements at each call. Defaults to 0.05.
-            rotation_norm ([type], optional): factor of normalization of rotation values, since the actions are of the same scale for each dimension. Defaults to 5..
-        """
-        pass
-
     def reinit(self):
         """
         Reinitialize the environment, e.g. when the gripper is broken during exploration
         """
-        pass
+        self.shutdown() # shutdown the original env first
+        self.__init__(self.headless)
 
     def reset(self, random_target = False):
         """
-        Reset the gripper position and the target position.
+        Reset the gripper position and set the target position.
         """
-        pass
+        # set target object
+        if random_target: # randomizd
+            pos = list(np.random.uniform(POS_MIN, POS_MAX)) # sample from uniform is valid range
+            self.target.set_position(pos) # random positio
+        else: # non-renadomized 
+            self.target.set_position(self.initial_target_positions) # fixed position
+        self.target.set_orientation([0, 0, 0])
+        self.pr.step()
+
+        # set end position to be initiliazed
+        self.agent.set_joint_positions(self.initial_joint_positions)
+        self.pr.step()
+        
+        # set collidable, for collision detection
+        self.gripper_left_pad.set_collidable(True) # set the pad of the gripper to be collidable, so as to check collision
+        self.target.set_collidable(True) # set the target to be collidable, so as to check collision
+        
+        # open the gripper if it's not fully open
+        if np.sum(self.gripper.get_open_amount()) < 1.5:
+            self.gripper.actuate(1.0, velocity=0.5)
+            self.pr.step()
+
+        # return current state of the env
+        return self._get_state()
+
 
     def step(self, action):
         """
         Move the robot arm according to the action.
-        If control_mode == 'joint_velocity', action is 7 dim of joint velocity values + 1 dim rotation of gripper
-        If control_mode == 'end_position', action is 3 dim of tip (end of robot arm) position values + 1 dim rotation of gripper
 
         Args:
-            action ([type]): [description]
+            action (np.array): Action is 7 dim of joint velocity values + 1 dim rotation of gripper
         """     
-        pass
+        # initialization
+        done = False # episode finishes
+        reward = 0
+        hold_flag = False # holding the object or not
+
+        if action is None or action.shape[0] != 7: # check if action is valid
+            print('No actions or wrong action dimensions')
+            action = list(np.random.uniform(-0.1, 0.1, 7)) # random action
+        self.agent.set_joint_target_velocities(action)
+        self.pr.step()
+
+        # get gripper position and check if its broken, if broken, reinitialize the environment and finish the episode
+        ax, ay, az = self.gripper.get_position()
+        if math.isnan(ax): # capture the broken gripper cases during exploration
+            print('Gripper position is nan.')
+            self.reinit()
+            done = True # end of episode
+
+        # get target position and calculate the distance between it and the gripper
+        tx, ty, tz = self.target.get_position()
+        offset = 0.08 # augmented reward: offset of target position above the target object
+        sqr_distance = (ax - tx) ** 2 + (ay - ty) ** 2 + (az - (tz + offset)) ** 2  # squared distance between the gripper and the target object
+        
+        # close the gripper if close enough to the object and the object is detected with the proximity sensor
+        if sqr_distance < 0.1 and self.proximity_sensor.is_detected(self.target) == True:
+            # make sure the gripper is open before grasping
+            self.gripper.actuate(1.0, velocity=0.5) # 1, open gripper
+            self.pr.step() # step simulation, after each movement
+            self.gripper.actuate(0.0, velocity=0.5) # 0, close gripper, velocity ensures to close with in one frame
+            self.pr.step() # step simulation, after each movement
+
+            if self._is_holding():
+                reward += self.reward_offset # extra reward for grasping the object
+                done = True # end of episode
+                hold_flag = True # object is grasped
+            else:
+                self.gripper.actuate(1.0, velocity=0.5)
+                self.pr.step()
+        elif np.sum(self.gripper.get_open_amount()) < 1.5: # if gripper is closed (not fully open) due to collision or else, open it; get_open_amount() return list of gripper joint values
+            self.gripper.actuate(1.0, velocity=0.5)
+            self.pr.step()
+        else:
+            pass
+
+        # the base reward is negative distance to target
+        reward -= np.sqrt(sqr_distance)
+
+        # case when the object fall off the table
+        if tz < self.initial_target_positions[2] - self.fall_down_offset:
+            done = True # end episode
+            reward = -self.reward_offset
+
+        # capture the cases of numerical problems
+        if math.isnan(reward): 
+            reward = 0.
+
+        return self._get_state(), reward, done, {'finished': hold_flag}
 
     def shutdown(self):
         """
         Close the simulator
         """
-        pass
-
+        self.pr.stop()
+        self.pr.shutdown()
